110168461041c63f60058d0688650608c1555ac8
VIKAS CHAUHAN <55353384+vikaschauhan07@users.noreply.github.com>
2023-10-10 16:44:27 +0530

    Add files via upload

---
+++ Crm/model-module/src/main/java/Crm/modelmodule/ModelModuleApplication.java
@@ -0,0 +1,13 @@
+package Crm.modelmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelModuleApplication.class, args);
+	}
+
+}
