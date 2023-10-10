8afe74048baaafe052b45b41e611b2ec9121d515
VIKAS CHAUHAN <55353384+vikaschauhan07@users.noreply.github.com>
2023-10-10 16:43:40 +0530

    Add files via upload

---
+++ Crm/repository-module/src/main/java/Crm/repositorymodule/RepositoryModuleApplication.java
@@ -0,0 +1,13 @@
+package Crm.repositorymodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryModuleApplication.class, args);
+	}
+
+}
---
+++ Crm/repository-module/src/test/java/Crm/repositorymodule/RepositoryModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package Crm.repositorymodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
